ec7d946d6707765b0485981bc36c07a23cd9a87a
kowalowy1234 <kowalowy1234@gmail.com>
2026-06-01 20:42:20 +0200

    generate auth and user modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,18 @@ import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env', '.env.development.local']
     }),
+    AuthModule,
+    UserModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
