ab1ba4141c8b1be3e32cf1bb58bab135740d0ebb
kowalowy1234 <kowalowy1234@gmail.com>
2026-06-03 20:32:58 +0200

    fix prisma module

--- src/shared/modules/prisma/prisma.service.ts
+++ src/shared/modules/prisma/prisma.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../../../prisma/src/generated/prisma/client'
 
 @Injectable()
 export class PrismaService extends PrismaClient {
@@ -10,9 +10,5 @@ export class PrismaService extends PrismaClient {
         });
 
         super({ adapter })
-    }
-
-    get prisma() {
-        return this.prisma
     }
 }
